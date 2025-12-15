7b7588b34c1e059d13fa7707dc45d06568f61480
Anwealso <48880195+Anwealso@users.noreply.github.com>
2025-12-15 21:16:49 +1100

    Reworked native app with manual reinstall

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,7 +1,6 @@
-const { getDefaultConfig } = require('expo/metro-config')
-
-const config = getDefaultConfig(__dirname)
-
-config.resolver.sourceExts.push('cjs')
-
-module.exports = config
+module.exports = {
+  resolver: {
+    sourceExts: ['js', 'jsx', 'json', 'ts', 'tsx', 'cjs', 'mjs'],
+    assetExts: ['glb', 'gltf', 'png', 'jpg'],
+  },
+}
